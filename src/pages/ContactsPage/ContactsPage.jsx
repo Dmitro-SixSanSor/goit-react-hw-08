bfe9afe35ba803a0093caa3e7e44c3b24ecc6ac2
Dmitro-SixSanSor <dmytro.ostaphiychuk@gmail.com>
2025-06-09 21:50:55 +0300

    add markup header

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { fetchContacts } from '../../redux/contacts/operations';
-import { selectToken } from '../../redux/auth/selectors';
-import { setAuthHeader } from '../../services/api';
-
 import ContactForm from '../../components/ContactForm/ContactForm';
 import ContactList from '../../components/ContactList/ContactList';
 import SearchBox from '../../components/SearchBox/SearchBox';
@@ -11,14 +8,10 @@ import styles from './ContactsPage.module.css';
 
 const ContactsPage = () => {
   const dispatch = useDispatch();
-  const token = useSelector(selectToken);
 
   useEffect(() => {
-    if (token) {
-      setAuthHeader(token);
-      dispatch(fetchContacts());
-    }
-  }, [dispatch, token]);
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <div className={styles.container}>
